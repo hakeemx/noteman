3073ea3f7d0cab73f85cb5190e9b15c2ed59d789
thechocolateman <hakeemjhamal@gmail.com>
2018-01-23 16:03:18 -0500

    Push wireframe

---
+++ src/components/Sidebar.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Sidebar extends Component {
+  render() {
+    return <div className="fl w-25 br b--black-05 vh-100 sidebar">Sidebar</div>;
+  }
+}
